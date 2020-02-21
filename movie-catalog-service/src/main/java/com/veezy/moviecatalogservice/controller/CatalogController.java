6b9f2971e28c604e0e01a0bea65d60533aa30d5b
Victor Bassey <viktorbasi545@gmail.com>
2020-02-21 19:56:44 +0100

    Hard code ratings in the movie catalog

--- movie-catalog-service/src/main/java/com/veezy/moviecatalogservice/controller/CatalogController.java
+++ movie-catalog-service/src/main/java/com/veezy/moviecatalogservice/controller/CatalogController.java
@@ -1,12 +1,14 @@
 package com.veezy.moviecatalogservice.controller;
 
 import com.veezy.moviecatalogservice.model.CatalogItem;
+import com.veezy.moviecatalogservice.model.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -14,6 +16,14 @@ public class CatalogController {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(new CatalogItem("Test", "Test Desc", 4));
+        List<Rating> ratingsList = Arrays.asList(
+                new Rating("1234", 3),
+                new Rating("5678", 4)
+        );
+
+        return ratingsList.stream()
+                .map(rating -> new CatalogItem("Name", "Desc", rating.getRating()))
+                .collect(Collectors.toList());
+
     }
 }
