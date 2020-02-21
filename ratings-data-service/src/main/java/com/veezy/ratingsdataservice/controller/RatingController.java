05bf555e1fe398f03e1d997a8e6e7f82aebcfc58
Victor Bassey <viktorbasi545@gmail.com>
2020-02-21 19:42:16 +0100

    Add ratings data service API

---
+++ ratings-data-service/src/main/java/com/veezy/ratingsdataservice/controller/RatingController.java
@@ -0,0 +1,16 @@
+package com.veezy.ratingsdataservice.controller;
+
+import com.veezy.ratingsdataservice.model.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingController {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+}
