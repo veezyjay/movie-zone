1228e22aa8bf95478491b6138fe64fd583d1af68
Victor Bassey <viktorbasi545@gmail.com>
2020-02-22 08:26:12 +0100

    Improve call to ratings data API

--- movie-catalog-service/src/main/java/com/veezy/moviecatalogservice/controller/CatalogController.java
+++ movie-catalog-service/src/main/java/com/veezy/moviecatalogservice/controller/CatalogController.java
@@ -3,6 +3,7 @@ package com.veezy.moviecatalogservice.controller;
 import com.veezy.moviecatalogservice.model.CatalogItem;
 import com.veezy.moviecatalogservice.model.Movie;
 import com.veezy.moviecatalogservice.model.Rating;
+import com.veezy.moviecatalogservice.model.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +19,13 @@ import java.util.stream.Collectors;
 public class CatalogController {
 
     @Autowired
-    RestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        List<Rating> ratingsList = Arrays.asList(
-                new Rating("1234", 3),
-                new Rating("5678", 4)
-        );
+        UserRating userRating = restTemplate.getForObject("http://localhost:8083/ratingsdata/user/" + userId, UserRating.class);
 
-        return ratingsList.stream()
+        return userRating.getRatings().stream()
                 .map(rating -> {
                     Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
                     return new CatalogItem(movie.getName(), "Desc", rating.getRating());
--- ratings-data-service/src/main/java/com/veezy/ratingsdataservice/controller/RatingController.java
+++ ratings-data-service/src/main/java/com/veezy/ratingsdataservice/controller/RatingController.java
@@ -1,6 +1,7 @@
 package com.veezy.ratingsdataservice.controller;
 
 import com.veezy.ratingsdataservice.model.Rating;
+import com.veezy.ratingsdataservice.model.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +13,13 @@ public class RatingController {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
     }
 }
