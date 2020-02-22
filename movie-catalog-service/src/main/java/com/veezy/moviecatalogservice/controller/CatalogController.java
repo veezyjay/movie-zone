7eece7fc569afdfdc84a3accdd7574ac4abdc66d
Victor Bassey <viktorbasi545@gmail.com>
2020-02-22 12:39:55 +0100

    Get movie details from external API

--- movie-catalog-service/src/main/java/com/veezy/moviecatalogservice/controller/CatalogController.java
+++ movie-catalog-service/src/main/java/com/veezy/moviecatalogservice/controller/CatalogController.java
@@ -28,7 +28,7 @@ public class CatalogController {
         return userRating.getRatings().stream()
                 .map(rating -> {
                     Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-                    return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
                 })
                 .collect(Collectors.toList());
 
--- movie-info-service/src/main/java/com/veezy/movieinfoservice/controller/MovieController.java
+++ movie-info-service/src/main/java/com/veezy/movieinfoservice/controller/MovieController.java
@@ -1,17 +1,29 @@
 package com.veezy.movieinfoservice.controller;
 
 import com.veezy.movieinfoservice.model.Movie;
+import com.veezy.movieinfoservice.model.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
 
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-        return new Movie(movieId, "Name for ID " + movieId);
+        MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" +
+                movieId + "?api_key=" +  apiKey, MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
 
     }
 }
